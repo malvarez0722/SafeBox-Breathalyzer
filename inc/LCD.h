9084fcd3c7754b93d440a1fc24978c64a2fbdbea
emiliocantu20 <47613914+emiliocantu20@users.noreply.github.com>
2020-12-02 15:37:59 -0700

    Add files via upload

---
+++ inc/LCD.h
@@ -0,0 +1,33 @@
+
+
+
+#ifndef _LCDH_
+#define _LCDH_
+#include <stdint.h>
+
+//------------OpenBox------------
+// Open Animation
+// Input: none
+// Output: none
+void OpenBox(void);
+
+//------------CloseBox------------
+// Close Animation
+// Input: none
+// Output: none
+void CloseBox(void);
+
+//------------StateBox------------
+// Prints the State of the Box into the LCD
+// Input: none
+// Output: none
+void StateBox(void);
+
+//------------TestBox------------
+// Uses switches to test the LCD
+// Input: none
+// Output: none
+void TestBox(void);
+
+
+#endif
